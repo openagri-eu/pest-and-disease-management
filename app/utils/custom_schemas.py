0fdd463646eaecc8958cb043284d770dcb02f5a0
stefan.drobic <stefan.drobic@vizlore.com>
2024-11-27 14:10:11 +0100

    updated to pest-risk example v2 jsonld format for response on /tool/ apis

--- app/utils/custom_schemas.py
+++ app/utils/custom_schemas.py
@@ -1,13 +1,3 @@
-context = {
-    "ocsm": "https://w3id.org/ocsm/",
-    "fsm": "http://www.farmtopia.com/ontology/farmtopia#",
-    "foodie": "http://foodie-cloud.com/model/foodie#",
-    "saref": "https://saref.etsi.org/core/"
-}
-
-calculated_risk = {
-    "@id": "urn:openagri:pestPrediction:{}", #uuid.uuid4()
-    "@type": "fsm:PestInfestationRisk",
-    "saref:hasTimestamp": "{}",
-    "ocsm:hasRiskLevel": "{}" # Low, Medium, High
-}
+context = [
+    "https://w3id.org/ocsm/main-context.jsonld"
+]
